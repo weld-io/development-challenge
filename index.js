106007693b1a59c80298ed69294c2d3a3c5809a9
Henric Malmberg <henric@weld.io>
2018-04-11 12:48:48 +0200

    use cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
 
 app.get('*', (req, res) => res.json({name:'Donald Duck'}));
 app.post('*', (req, res) => res.sendStatus(201));
 app.put('*', (req, res) => res.sendStatus(204));
 app.delete('*', (req, res) => res.sendStatus(204));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port ' + process.env.PORT || 3000));
