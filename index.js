2d19ad4e67402dec27ceeb70ec79c1318d7f067d
Henric Malmberg <henric@weld.io>
2018-07-17 14:06:57 +0200

    fix bug where port number was not shown for running server

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.post('*', (req, res) => res.sendStatus(201));
 app.put('*', (req, res) => res.sendStatus(204));
 app.delete('*', (req, res) => res.sendStatus(204));
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port ' + process.env.PORT || 3000));
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port ' + (process.env.PORT || 3000)));
